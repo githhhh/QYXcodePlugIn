27b6599e5514e088376d4276aa97750187267da8
唐斌 <bin.tang@qyer.com>
2015-09-22 17:30:45 +0800

    update code

---
+++ QYFileTemplate/Singleton file.xctemplate/GCDSingletonObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,49 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___()
+
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
++ (id)sharedSingleton
+{
+    static ___FILEBASENAMEASIDENTIFIER___ *_sharedSingleton;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedSingleton = [[self alloc] init];
+    });
+    return _sharedSingleton;
+}
+
+
+-(id)init{
+    
+    self = [super init];
+    if (self) {
+        
+        
+        
+        
+    }
+    return self;
+}
+
+
+
+//-(void)dealloc{
+//
+//}
+
+
+
+@end
