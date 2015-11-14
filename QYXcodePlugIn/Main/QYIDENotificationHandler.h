80147bcb4fdae3ecc4d37d5ee4133e70785920d3
唐斌 <bin.tang@qyer.com>
2015-11-14 23:31:15 +0800

    update code

---
+++ QYXcodePlugIn/Main/QYIDENotificationHandler.h
@@ -0,0 +1,19 @@
+//
+//  QYIDENotificationHandler.h
+//  QYXcodePlugIn
+//
+//  Created by 唐斌 on 15/9/22.
+//  Copyright (c) 2015年 X.Y. All rights reserved.
+//
+#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
+
+@interface QYIDENotificationHandler : NSObject
++ (id)sharedHandler;
+
+@property (nonatomic,retain)id globlaParamter;
+
+@property (nonatomic,retain)NSMenuItem* geterMenuItem;
+@property (nonatomic,retain)NSMenuItem* requestVerifiMenuItem;
+@property (nonatomic,retain)NSMenuItem* settingsMenuItem;
+@end
---
+++ QYXcodePlugIn/Main/QYWindowsCloseProtocol.h
@@ -0,0 +1,13 @@
+//
+//  QYWindowsCloseProtocol.h
+//  QYXcodePlugIn
+//
+//  Created by 唐斌 on 15/11/12.
+//  Copyright © 2015年 X.Y. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol QYWindowsCloseProtocol <NSObject>
+-(void)windowDidClose;
+@end
